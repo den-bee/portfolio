3af5c5c67eff9eadd2ea6cad724ff417b871b84f
Michiel Baert <michiel.baert@student.ap.be>
2024-10-27 10:09:16 +0100

    fix responsive design on large screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,10 +7,12 @@ const Home = () => {
       <section className="flex flex-col justify-center gap-12 xl:flex-row xl:flex-wrap md:justify-start md:text-left">
         <div className="w-fit flex flex-col justify-center">
           <div className="pt-10 flex flex-col justify-center">
-            <p className="text-xl">Hello, my name is</p>
-            <p className="text-4xl sm:text-5xl text-orange">Michiel Baert</p>
+            <p className="text-xl 2xl:text-3xl">Hello, my name is</p>
+            <p className="text-4xl sm:text-5xl 2xl:text-6xl text-orange">
+              Michiel Baert
+            </p>
           </div>
-          <div className="flex flex-col gap-5 p-10 sm:pl-0 text-lg sm:text-3xl">
+          <div className="flex flex-col gap-5 p-10 sm:pl-0 text-lg sm:text-3xl 2xl:text-4xl 3xl:text-5xl">
             <p>
               I'm a passionate
               <span className="text-orange"> front-end developer</span> who
@@ -25,7 +27,7 @@ const Home = () => {
         <div className="w-72 xl:block hidden">
           <img className="absolute" src={laptop} />
         </div>
-        <div className="w-fit flex flex-col self-center md:self-start text-5xl">
+        <div className="w-fit 2xl:w-full flex flex-col self-center md:self-start text-5xl 2xl:text-7xl">
           <p>Check out my</p>
 
           <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         lg: "1024px",
         xl: "1280px",
         "2xl": "1536px",
+        "3xl": "1930px",
       },
       backgroundImage: {
         "green-pattern": "url('images/background.png')",
